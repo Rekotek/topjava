b1a4f0c82ed767df70b7b07952795efb33b89a0f
Taras Tkachenko <TarasTkachenko@me.com>
2018-11-05 14:41:39 +0200

    Make HSQLDB JDBC implementation works

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/PostgresMealRepositoryImpl.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
+
+import java.time.LocalDateTime;
+
+@Profile(Profiles.POSTGRES_DB)
+@Repository
+public class PostgresMealRepositoryImpl extends JdbcMealBaseRepository {
+
+    @Autowired
+    public PostgresMealRepositoryImpl(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    protected LocalDateTime amendDateTime(LocalDateTime localDateTime) {
+        return localDateTime;
+    }
+}
