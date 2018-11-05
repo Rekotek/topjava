d0e61021e05f4ff96a818181abc88669e4bcfa7f
Taras Tkachenko <TarasTkachenko@me.com>
2018-11-05 20:43:49 +0200

    Duplicate @Test at override methods for clarify.

--- src/test/java/ru/javawebinar/topjava/service/datajpa/UserServiceDataJpaTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/UserServiceDataJpaTest.java
@@ -1,52 +1,64 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.junit.Test;
+import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.service.UserServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 @ActiveProfiles(Profiles.DATAJPA)
 public class UserServiceDataJpaTest extends UserServiceTest {
 
+    @Test
     @Override
     public void create() throws Exception {
         super.create();
     }
 
+    @Test(expected = DataAccessException.class)
     @Override
     public void duplicateMailCreate() throws Exception {
         super.duplicateMailCreate();
     }
 
+    @Test
     @Override
     public void delete() throws Exception {
         super.delete();
     }
 
+    @Test(expected = NotFoundException.class)
     @Override
     public void deletedNotFound() throws Exception {
         super.deletedNotFound();
     }
 
+    @Test
     @Override
     public void get() throws Exception {
         super.get();
     }
 
+    @Test(expected = NotFoundException.class)
     @Override
     public void getNotFound() throws Exception {
         super.getNotFound();
     }
 
+    @Test
     @Override
     public void getByEmail() throws Exception {
         super.getByEmail();
     }
 
+    @Test
     @Override
     public void update() throws Exception {
         super.update();
     }
 
+    @Test
     @Override
     public void getAll() throws Exception {
         super.getAll();
--- src/test/java/ru/javawebinar/topjava/service/jdbc/MealServiceJdbcTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/MealServiceJdbcTest.java
@@ -1,51 +1,61 @@
 package ru.javawebinar.topjava.service.jdbc;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.service.MealServiceTest;
 
 @ActiveProfiles(Profiles.JDBC)
 public class MealServiceJdbcTest extends MealServiceTest {
+    @Test
     @Override
     public void delete() throws Exception {
         super.delete();
     }
 
+    @Test
     @Override
     public void deleteNotFound() throws Exception {
         super.deleteNotFound();
     }
 
+    @Test
     @Override
     public void create() throws Exception {
         super.create();
     }
 
+    @Test
     @Override
     public void get() throws Exception {
         super.get();
     }
 
+    @Test
     @Override
     public void getNotFound() throws Exception {
         super.getNotFound();
     }
 
+    @Test
     @Override
     public void update() throws Exception {
         super.update();
     }
 
+    @Test
     @Override
     public void updateNotFound() throws Exception {
         super.updateNotFound();
     }
 
+    @Test
     @Override
     public void getAll() throws Exception {
         super.getAll();
     }
 
+    @Test
     @Override
     public void getBetween() throws Exception {
         super.getBetween();
